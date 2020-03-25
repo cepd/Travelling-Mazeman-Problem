9525864952258a842512b67f808817ab68bc5e2c
segara2410 <segara2410@gmail.com>
2020-03-25 11:06:56 +0700

    update the tmp code

--- src/travellingmazeman.py
+++ src/travellingmazeman.py
@@ -1,132 +1,204 @@
 import queue
+import networkx as nx
 
+def mazeToGraph(maze):
+  graph = nx.Graph()
+  all_path = {}
+  for i in range(0, 4):
+    key = chr(ord('A') + i)
+    all_path['S' + key] = findPath(maze, 'S', key)
+    all_path[key + 'S'] = findPath(maze, key, 'S')
+    graph.add_edge(key, 'S', weight=len(all_path['S' + key]))
+
+    all_path['F' + key] = findPath(maze, 'F', key)
+    all_path[key + 'F'] = findPath(maze, key, 'F')
+    graph.add_edge(key, 'F', weight=len(all_path[key + 'F']))
+    
+    for j in range(3 - i):
+      target = chr(ord(key) + j + 1)
+      
+      all_path[key + target] = findPath(maze, key, target)
+      all_path[target + key] = findPath(maze, target, key)
+      graph.add_edge(key, target, weight=len(all_path[key + target]))
+      
+  return graph, all_path
+
+def getTotalCost(graph, condition):
+  total_cost = int(graph['S'][condition[0]]['weight']) + int(graph[condition[len(condition) - 1]]['F']['weight'])
+  for index in range(len(condition) - 1):
+    total_cost = total_cost + int(graph[condition[index]][condition[index + 1]]['weight'])
+  return total_cost
+
+def evaluate(graph, initial_condition):
+  local_minimum_cost = getTotalCost(graph, initial_condition)
+  local_minimum_condition = initial_condition.copy()
+  local_minimum_changed = False
+
+  first_index = 0
+
+  while first_index in range(len(initial_condition) - 1):
+    second_index = first_index + 1
+    while second_index < len(initial_condition):
+      modified_condition = initial_condition.copy()
+      modified_condition[first_index], modified_condition[second_index] = initial_condition[second_index], initial_condition[first_index]
+      modified_condition_cost = getTotalCost(graph, modified_condition)
+
+      if modified_condition_cost < local_minimum_cost:
+        local_minimum_cost = modified_condition_cost
+        local_minimum_condition = modified_condition.copy()
+        local_minimum_changed = True
+
+      second_index = second_index + 1
+    first_index = first_index + 1
+
+  if local_minimum_changed:
+    return evaluate(graph, local_minimum_condition)
+
+  return local_minimum_condition, local_minimum_cost
 
 def createMaze():
-    maze = []
-    maze.append(["#","#", "#", "#", "#", "O","#"])
-    maze.append(["#"," ", " ", " ", "#", " ","#"])
-    maze.append(["#"," ", "#", " ", "#", " ","#"])
-    maze.append(["#"," ", "#", " ", " ", " ","#"])
-    maze.append(["#"," ", "#", "#", "#", " ","#"])
-    maze.append(["#"," ", " ", " ", "#", " ","#"])
-    maze.append(["#","#", "#", "#", "#", "X","#"])
+  maze = []
+  maze.append(['#', '#', '#', '#', '#', 'S', '#', '#', '#'])
+  maze.append(['#', ' ', 'D', ' ', ' ', ' ', ' ', ' ', '#'])
+  maze.append(['#', ' ', '#', '#', ' ', '#', '#', 'C', '#'])
+  maze.append(['#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'])
+  maze.append(['#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'])
+  maze.append(['#', ' ', '#', ' ', '#', 'B', '#', ' ', '#'])
+  maze.append(['#', ' ', '#', ' ', '#', ' ', '#', '#', '#'])
+  maze.append(['#', 'A', ' ', ' ', ' ', ' ', ' ', ' ', '#'])
+  maze.append(['#', '#', '#', '#', '#', '#', '#', 'F', '#'])
 
-    return maze
+  return maze
 
-def createMaze2():
-    maze = []
-    maze.append(["#","#", "#", "#", "#", "O", "#", "#", "#"])
-    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
-    maze.append(["#"," ", "#", "#", " ", "#", "#", " ", "#"])
-    maze.append(["#"," ", "#", " ", " ", " ", "#", " ", "#"])
-    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
-    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
-    maze.append(["#"," ", "#", " ", "#", " ", "#", "#", "#"])
-    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
-    maze.append(["#","#", "#", "#", "#", "#", "#", "X", "#"])
+def printMaze(maze, start_pos, path):
+  for y in range (len(maze)):
+    for x, pos in enumerate(maze[y]):
+      if pos == start_pos:
+        starti = x
+        startj = y
+        break
 
-    return maze
+  i = starti
+  j = startj
+
+  pos = set()
+  for move in path:
+    if move == 'L':
+      i -= 1
+
+    elif move == 'R':
+      i += 1
+
+    elif move == 'U':
+      j -= 1
+
+    elif move == 'D':
+      j += 1
+    pos.add((j, i))
+  
+  for j, row in enumerate(maze):
+    for i, col in enumerate(row):
+      if (j, i) in pos:
+        print('+ ', end='')
+      else:
+        print(col + ' ', end='')
+    print()
+  print()
+
+def valid(maze, moves, start_pos):
+  for y in range (len(maze)):
+    for x, pos in enumerate(maze[y]):
+      if pos == start_pos:
+        starti = x
+        startj = y
+        break
+
+  i = starti
+  j = startj
+
+  for move in moves:
+    if move == 'L':
+      i -= 1
+
+    elif move == 'R':
+      i += 1
+
+    elif move == 'U':
+      j -= 1
+
+    elif move == 'D':
+      j += 1
+
+    if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):
+      return False
+    elif (maze[j][i] == '#'):
+      return False
+
+  return True
+
+def findEnd(maze, moves, start_pos, finish_pos):
+  for y in range (len(maze)):
+    for x, pos in enumerate(maze[y]):
+      if pos == start_pos:
+        starti = x
+        startj = y
+        break
+
+  i = starti
+  j = startj
+  for move in moves:
+    if move == 'L':
+      i -= 1
+
+    elif move == 'R':
+      i += 1
+
+    elif move == 'U':
+      j -= 1
+
+    elif move == 'D':
+      j += 1
 
 
-def printMaze(maze, path=""):
-    for x, pos in enumerate(maze[0]):
-        if pos == "O":
-            start = x
-
-    i = start
-    j = 0
-    pos = set()
-    for move in path:
-        if move == "L":
-            i -= 1
-
-        elif move == "R":
-            i += 1
-
-        elif move == "U":
-            j -= 1
-
-        elif move == "D":
-            j += 1
-        pos.add((j, i))
-    
-    for j, row in enumerate(maze):
-        for i, col in enumerate(row):
-            if (j, i) in pos:
-                print("+ ", end="")
-            else:
-                print(col + " ", end="")
-        print()
-        
-
-
-def valid(maze, moves):
-    for x, pos in enumerate(maze[0]):
-        if pos == "O":
-            start = x
-
-    i = start
-    j = 0
-    for move in moves:
-        if move == "L":
-            i -= 1
-
-        elif move == "R":
-            i += 1
-
-        elif move == "U":
-            j -= 1
-
-        elif move == "D":
-            j += 1
-
-        if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):
-            return False
-        elif (maze[j][i] == "#"):
-            return False
-
+  if maze[j][i] == finish_pos:
     return True
 
+  return False
 
-def findEnd(maze, moves):
-    for x, pos in enumerate(maze[0]):
-        if pos == "O":
-            start = x
+def findPath(maze, start_pos, finish_pos):
+  nums = queue.Queue()
+  nums.put('')
+  path = ''
+  while not findEnd(maze, path, start_pos, finish_pos): 
+    path = nums.get()
+    for j in ['L', 'R', 'U', 'D']:
+      next_path = path + j
+      if valid(maze, next_path, start_pos):
+        nums.put(next_path)
 
-    i = start
-    j = 0
-    for move in moves:
-        if move == "L":
-            i -= 1
-
-        elif move == "R":
-            i += 1
-
-        elif move == "U":
-            j -= 1
-
-        elif move == "D":
-            j += 1
-
-    if maze[j][i] == "X":
-        print("Found: " + moves)
-        printMaze(maze, moves)
-        return True
-
-    return False
+  return path
 
 
-# MAIN ALGORITHM
+all_path = {}
+maze  = createMaze()
 
-nums = queue.Queue()
-nums.put("")
-add = ""
-maze  = createMaze2()
+graph, all_path = mazeToGraph(maze)
+local_minimum_condition = []
+local_minimum_condition.append('S')
+local_minimum_objectives, local_minimum_cost = evaluate(graph, ['A', 'B', 'C', 'D'])
+for objective in local_minimum_objectives:
+  local_minimum_condition.append(objective)
 
-while not findEnd(maze, add): 
-    add = nums.get()
-    #print(add)
-    for j in ["L", "R", "U", "D"]:
-        put = add + j
-        if valid(maze, put):
-            nums.put(put)
+local_minimum_condition.append('F')
+print('minimum steps found :', local_minimum_cost)
+
+for path in local_minimum_condition:
+  if path == 'F':
+    print(path)
+  else:
+    print(path, end=' -> ')
+print()
+
+for i in range (int(len(local_minimum_condition)) - 1) :
+  print('Step', i + 1, ' : ', local_minimum_condition[i], '->', local_minimum_condition[i + 1])
+  printMaze(maze, local_minimum_condition[i], all_path[local_minimum_condition[i] + local_minimum_condition[i + 1]])
